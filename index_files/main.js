3cf2f42b84e51ad3eafd8c2e05ff49dc9cb9f8c3
Kamrul <kamrul.ahsan.rocky@gmail.com>
2018-04-22 22:21:00 +0600

    first commit

---
+++ index_files/main.js
@@ -0,0 +1,21 @@
+// (function ($) {
+//     "use strict";
+//     $('body').css("overflow", "hidden");
+//     function handlerIn() {
+//         $('body').css("overflow", "scroll");
+//     }
+//     function handlerOut() {
+//         $('body').css("overflow", "hidden");
+//     }
+//     $('.main-menu ul li ul.submenu li>ul.submenu li').each(function () {
+//         if ($(this).children('.submenu').length > 0) {
+//             $(this).hover(handlerIn, handlerOut)
+//         }
+//     }
+//     );
+
+//     $('nav>ul>li:nth-child(3)').hover(handlerIn, handlerOut);
+
+// }
+
+// )(jQuery);
